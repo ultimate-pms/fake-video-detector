e971f6093eca729ce7fd52b89df9b86f0bec48d3
David <david@nedved.com.au>
2018-08-02 09:15:52 +1000

    path bugfixes for no-docker support

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 #
 # Setup on local ubuntu machine without docker...
 
+apt-get update -q
 apt-get install imagemagick ffmpeg -y
 
-apt-get update -q
 apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libgd2-dev libpuzzle-bin
 apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" build-essential
 apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libtool m4 automake
